dde466ec763a919a56c64dd2586f8cb0fee75895
luismoreno89 <ing.moreno.89@gmail.com>
2026-09-19 22:39:27 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-dinero-v3-6';
+const CACHE='mi-dinero-v3-6-2';
 const ASSETS=['./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./brand-logo.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
