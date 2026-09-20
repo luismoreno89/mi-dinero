22d0cb5e743e6ee8c8670e6ef566a1079e3b7fb8
luismoreno89 <ing.moreno.89@gmail.com>
2026-09-19 22:32:04 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE='mi-dinero-v3-3';
-const ASSETS=['./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='mi-dinero-v3-6';
+const ASSETS=['./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./brand-logo.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{
  if(e.request.mode==='navigate')e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
- else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+ else e.respondWith(fetch(e.request).then(r=>{if(r&&r.ok){const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c))}return r}).catch(()=>caches.match(e.request)));
 });
